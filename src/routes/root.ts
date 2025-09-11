a4f27d7728d10416cc61ae0fe24acc2672df87a6
Leandro Pereira <leandro.pereiran23@gmail.com>
2025-09-10 21:47:05 -0300

    Add root.ts

---
+++ src/routes/root.ts
@@ -0,0 +1,23 @@
+import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
+
+const rootRoutes: FastifyPluginAsyncTypebox = async (fastify) => {
+    fastify.get('/', {
+        schema: {
+            tags: ["root"],
+            summary: "Obtiene un token JWT",
+            description: "Autentica usuario y contraseña, devuelve un token JWT si es correcto",
+            response: {
+                200: {
+                    type: 'object',
+                    properties: {
+                        ping: { type: 'string' }
+                    }
+                }
+            }
+        },
+    }, async (req, res) => {
+        return { ping: 'ok' };
+    });
+}
+
+export default rootRoutes;
